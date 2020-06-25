7e22044fff59045d28c906f246826acf22711a03
Priya Singh <63637974+PriyaSingh01mar@users.noreply.github.com>
2020-06-25 11:39:52 +0530

    Update Laguage_Translator.py

--- Desktop/Small Projects/Laguage_Translator.py
+++ Desktop/Small Projects/Laguage_Translator.py
@@ -1,12 +1,12 @@
-'''Some sample of the langauages in which you can convert your text
+from translate import Translator
+def language():
+    '''Sample of the langauages in which you can convert your text
 German
 CHINESE
 Hindi
-TELUGU
-TURKISH
-'''
-
-from translate import Translator
-translator= Translator(to_lang=input("Enter the language in which you want to convert: "))
-translation = translator.translate(input("Enter the text to convert : "))
-print(translation)
+    '''
+    translator= Translator(to_lang=input("Enter the language in which you want to convert: "))
+    translation = translator.translate(input("Enter the text to convert : "))
+    print(translation)
+print(language.__doc__)
+language()
